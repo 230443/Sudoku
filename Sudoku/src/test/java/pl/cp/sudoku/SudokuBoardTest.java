a1ca0975ee544ff7bd10546fb49e68eebc7b7a49
Daniel Baraniak <230443@edu.p.lodz.pl>
2021-03-10 10:59:36 +0100

    Empty test added.

---
+++ Sudoku/src/test/java/pl/cp/sudoku/SudokuBoardTest.java
@@ -0,0 +1,26 @@
+package pl.cp.sudoku;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import org.junit.jupiter.api.Test;
+import pl.cp.sudoku.SudokuBoard;
+
+/**
+ * @author daniel
+ */
+public class SudokuBoardTest {
+
+    public SudokuBoardTest() {
+    }
+
+    /**
+     * Test of append method, of class StudentArrayList.
+     */
+    @Test
+    public void fillBoardTest() {
+
+    }
+
+}
