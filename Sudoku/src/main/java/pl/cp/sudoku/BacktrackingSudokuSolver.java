024b507541724db8c593a7ea6432e8898aa941e7
230447 Adam Czyżewski <aczyzews@gmail.com>
2021-03-16 20:49:16 +0100

    Created template for solve method

---
+++ Sudoku/src/main/java/pl/cp/sudoku/BacktrackingSudokuSolver.java
@@ -0,0 +1,17 @@
+package pl.cp.sudoku;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Backtracking Sudoku Solver.
+ */
+
+class BacktrackingSudokuSolver implements SudokuSolver {
+
+    @Override
+    public boolean solve(SudokuBoard board) {
+        return true;
+    }
+}
