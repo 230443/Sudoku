d42559d71b1f333b784bc1291c8e517eef875b89
Daniel Baraniak <230443@edu.p.lodz.pl>
2021-03-23 19:35:21 +0100

    added test for sudokuBoardElement, changed accessors names to comply with future javaBeans

---
+++ Sudoku/src/test/java/pl/cp/sudoku/SudokuFieldTest.java
@@ -0,0 +1,22 @@
+package pl.cp.sudoku;
+
+import org.junit.jupiter.api.Test;
+import pl.cp.sudoku.elements.SudokuField;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SudokuFieldTest {
+
+    @Test
+    public void set_getTest() {
+
+        SudokuField field = new SudokuField();
+
+        field.setValue(1);
+        assertSame(1, field.getValue());
+
+    }
+
+
+}
+
+
