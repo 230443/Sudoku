18f0063f44ab40ecb6dd1e83d8bca5afdf694548
Daniel Baraniak <230443@edu.p.lodz.pl>
2021-04-13 14:45:02 +0200

    SudokuBoardElementTest splitted

--- Sudoku/src/test/java/pl/cp/sudoku/SudokuBoardElementTest.java
+++ Sudoku/src/test/java/pl/cp/sudoku/SudokuBoardElementTest.java
@@ -1,8 +1,11 @@
 package pl.cp.sudoku;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pl.cp.sudoku.elements.SudokuBox;
+import pl.cp.sudoku.elements.SudokuColumn;
 import pl.cp.sudoku.elements.SudokuField;
+import pl.cp.sudoku.elements.SudokuRow;
 
 import java.util.Arrays;
 
@@ -11,30 +14,64 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SudokuBoardElementTest {
 
-    @Test
-    public void verifyTest() throws Exception {
+    SudokuField[] field;
 
-        SudokuField[] field = new SudokuField[9];
-
+    public SudokuBoardElementTest() {
+        field = new SudokuField[9];
         for (int i = 0; i < 9; i++) {
             field[i] = new SudokuField();
+        }
+    }
+
+
+    @Test
+    public void verifyCorrectTest() throws Exception {
+        for (int i = 0; i < 9; i++) {
             field[i].setValue(i);
         }
-        SudokuBox box1 = new SudokuBox(Arrays.asList(field));
-        assertTrue(box1.verify());
+        SudokuBox box = new SudokuBox(Arrays.asList(field));
+        assertTrue(box.verify());
+    }
+
+    @Test
+    public void verifyIncorrectTest() throws Exception {
 
         for (int i = 0; i < 9; i++) {
             field[i].setValue(1);
         }
-        SudokuBox box2 = new SudokuBox(Arrays.asList(field));
-        assertFalse(box2.verify());
+        SudokuBox box = new SudokuBox(Arrays.asList(field));
+        assertFalse(box.verify());
+    }
+
+    @Test
+    public void verifyEmptyTest() throws Exception {
 
         for (int i = 0; i < 9; i++) {
             field[i].setValue(0);
         }
-        SudokuBox box3 = new SudokuBox(Arrays.asList(field));
-        assertTrue(box3.verify());
+        SudokuBox box = new SudokuBox(Arrays.asList(field));
+        assertTrue(box.verify());
+    }
 
+    @Test
+    public void notifyTest() throws Exception {
+
+        SudokuRow row = new SudokuRow();
+        SudokuColumn column = new SudokuColumn();
+        SudokuBox box = new SudokuBox();
+
+        SudokuField[] fields = new SudokuField[9];
+        for (int i = 0; i < 9; i++) {
+            fields[i] = new SudokuField(row, column, box);
+        }
+
+        for (int i = 0; i < 9; i++) {
+            fields[i].setValue(1);
+        }
+
+        for (int i = 0; i < 9; i++) {
+            assertFalse(fields[i].verify());
+        }
     }
 
 
