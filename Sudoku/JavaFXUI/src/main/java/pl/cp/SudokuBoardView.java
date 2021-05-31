1286af4fec22d043821ec3be9f48b8b686948c01
Daniel Baraniak <230443@edu.p.lodz.pl>
2021-05-31 23:54:34 +0200

    changing background color

--- Sudoku/JavaFXUI/src/main/java/pl/cp/SudokuBoardView.java
+++ Sudoku/JavaFXUI/src/main/java/pl/cp/SudokuBoardView.java
@@ -8,9 +8,8 @@ import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
-import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import pl.cp.sudoku.Difficulty;
@@ -33,6 +32,7 @@ public class SudokuBoardView {
 
         createAndConfigurePane();
     }
+
     private void createAndConfigurePane() {
         view = new GridPane();
 
@@ -45,34 +45,36 @@ public class SudokuBoardView {
         for (int i = 0; i < 9; i++) {
             view.getColumnConstraints().add(new ColumnConstraints(30));
             view.getRowConstraints().add(new RowConstraints(30));
-            for (int j = 0; j < 9 ; j++) {
+            for (int j = 0; j < 9; j++) {
                 if (tmpBoard[i][j] == 0) {
-                    TextField textField=new TextField();
-                    int tmpi=i;
-                    int tmpj=j;
+                    TextField textField = new TextField();
+                    int tmpi = i;
+                    int tmpj = j;
                     textField.setTextFormatter(new TextFormatter<String>((TextFormatter.Change change) -> {
                         String newText = change.getControlNewText();
 
-                            if (check(newText)) {
+                        if (check(newText)) {
 
-                                return null;
-                            }
-                            else {
-                                if(newText.equals("")){
-                                model.set(tmpi,tmpj,0);
-                                    }
-                                else{
-                                model.set(tmpi,tmpj,Integer.parseInt(newText));
-                                System.out.print(tmpi+" "+tmpj+"\n");
-                                System.out.print(model.toString());
+                            return null;
+                        } else {
+                            if (newText.equals("")) {
+                                model.set(tmpi, tmpj, 0);
+                                textField.setStyle("-fx-control-inner-background: white");
+                            } else {
+                                if (model.set(tmpi, tmpj, Integer.parseInt(newText))) {
+                                    textField.setStyle("-fx-control-inner-background: green");
+                                } else {
+                                    textField.setStyle("-fx-control-inner-background: red");
                                 }
-
-
-                                return change;
+                                System.out.print(tmpi + " " + tmpj + "\n");
+                                System.out.print(model.toString());
                             }
+
+
+                            return change;
+                        }
 
                     }));
-
 
 
                     view.add(textField, j, i);
@@ -85,14 +87,13 @@ public class SudokuBoardView {
     }
 
 
-
-    private static boolean isNotNumeric(String str){
+    private static boolean isNotNumeric(String str) {
         return !(str.matches("[0-9]"));
     }
 
-    private static boolean check(String str){
+    private static boolean check(String str) {
         if (str.equals("")) return false;
-         return isNotNumeric(str);
+        return isNotNumeric(str);
     }
 
 }
